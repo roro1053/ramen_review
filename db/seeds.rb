bf1e5d7f1c945dc4cc62752961d428f74942d180
roro1053 <c1053rroro@gmail.com>
2021-01-30 21:11:08 +0900

    seedを登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+RamenShop.create(name: "天下二品")
+RamenShop.create(name: "角蘭")
+RamenShop.create(name: "たらふく")
+RamenShop.create(name: "吾郎")
